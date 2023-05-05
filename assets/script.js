9fe9a0a48b536fbc92b94a7d203b088cb485d6db
Anthonyshakurangelos <anthonyshakurangelos@gmail.com>
2023-05-04 20:23:50 -0600

    polished up app

--- assets/script.js
+++ assets/script.js
@@ -1,178 +1,155 @@
-var searchBtn = document.getElementById("search-btn");
+// const apiKey = '118c7f089309d40df508ea89da910c9e';
 
-var inputValue = document.querySelector("#inputValue");
-var nameEl = document.querySelector("#name");
-var desc = document.querySelector("#currentImg");
+// // Get DOM elements
+// const searchInput = document.getElementById('search-input');
+// const searchButton = document.getElementById('search-button');
+// const locationElement = document.getElementById('location');
+// const dateElement = document.getElementById('date');
+// const weatherIconElement = document.getElementById('weather-icon');
+// const temperatureElement = document.getElementById('temperature');
+// const windElement = document.getElementById('wind');
+// const humidityElement = document.getElementById('humidity');
+// const weatherDescriptionElement = document.getElementById('weather-description');
+// const forecastElement = document.getElementById('forecast');
 
-var currentTemp = document.querySelector("#currentTemp");
-var currentDay = document.querySelector("#current");
-var currentWind = document.querySelector("#currentWind");
-var currentHumidity = document.querySelector("#currentHumidity");
-var unhideDiv1 = document.getElementById("icons");
-var unhideDiv1 = document.getElementById("icons1");
-var unhideDiv2 = document.getElementById("icons2");
-var unhideDiv3 = document.getElementById("icons3");
-var unhideDiv4 = document.getElementById("icons4");
+// // Handle search button click event
+// searchButton.addEventListener('click', () => {
+//   getWeatherData(searchInput.value);
+// });
 
-var unhideDiv5 = document.getElementById("icons5");
-var city = [];
-var hideDiv = document.getElementById("weatherImg");
-var timeEl = $("#time-display");
-var apiKey = "118c7f089309d40df508ea89da910c9e";
-var lat;
-var lon;
+// // Get weather data from API
+// async function getWeatherData(location) {
+//   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
+//   const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=${apiKey}`;
 
-function displayTime() {
-  var now = dayjs().format("M/D/YYYY");
-  timeEl.text(now);
+//   try {
+//     const response = await fetch(apiUrl);
+//     const data = await response.json();
+//     const forecastResponse = await fetch(forecastApiUrl);
+//     const forecastData = await forecastResponse.json();
+//     displayWeatherData(data, forecastData);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// // Display weather data
+// function displayWeatherData(data, forecastData) {
+//   // Location and date
+//   locationElement.innerText = `${data.name}, ${data.sys.country}`;
+//   const date = new Date();
+//   dateElement.innerText = date.toDateString();
+
+//   // Weather icon
+//   const iconCode = data.weather[0].icon;
+//   const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+//   weatherIconElement.innerHTML = `<img src="${iconUrl}" alt="${data.weather[0].description}" />`;
+
+//   // Temperature, wind, and humidity
+//   const tempFahrenheit = (data.main.temp * 9/5) + 32;
+//   temperatureElement.innerHTML = `${data.main.temp.toFixed(1)}&#176;F`;
+//   windElement.innerText = `Wind: ${data.wind.speed} m/s`;
+//   humidityElement.innerText = `Humidity: ${data.main.humidity}%`;
+
+//   // Weather description
+//   weatherDescriptionElement.innerText = `${data.weather[0].description}`;
+
+//   // Forecast
+//   let forecastHtml = '';
+//   for (let i = 0; i < forecastData.list.length; i++) {
+//     const forecast = forecastData.list[i];
+//     const forecastDate = new Date(forecast.dt_txt);
+//     if (forecastDate.getHours() === 12) {
+//       const forecastIconCode = forecast.weather[0].icon;
+//       const forecastIconUrl = `https://openweathermap.org/img/w/${forecastIconCode}.png`;
+//       const forecastTemperature = forecast.main.temp.toFixed(1);
+//       forecastHtml += `<div class="forecast-item">
+//                           <div class="forecast-date">${forecastDate.toDateString()}</div>
+//                           <div class="forecast-icon"><img src="${forecastIconUrl}" alt="${forecast.weather[0].description}" /></div>
+//                           <div class="forecast-temperature">${forecastTemperature}&#176;C</div>
+//                         </div>`;
+//     }
+//   }
+//   forecastElement.innerHTML = forecastHtml;
+// }
+
+const apiKey = '118c7f089309d40df508ea89da910c9e';
+
+// Get DOM elements
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const locationElement = document.getElementById('location');
+const dateElement = document.getElementById('date');
+const weatherIconElement = document.getElementById('weather-icon');
+const temperatureElement = document.getElementById('temperature');
+const windElement = document.getElementById('wind');
+const humidityElement = document.getElementById('humidity');
+const weatherDescriptionElement = document.getElementById('weather-description');
+const forecastElement = document.getElementById('forecast');
+
+// Handle search button click event
+searchButton.addEventListener('click', () => {
+  getWeatherData(searchInput.value);
+});
+
+// Get weather data from API
+async function getWeatherData(location) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${apiKey}`;
+  const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=imperial&appid=${apiKey}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const forecastResponse = await fetch(forecastApiUrl);
+    const forecastData = await forecastResponse.json();
+    displayWeatherData(data, forecastData);
+
+    // Save search to local storage
+    let searches = JSON.parse(localStorage.getItem('searches')) || [];
+    searches.push(location);
+    localStorage.setItem('searches', JSON.stringify(searches));
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-displayTime();
+// Display weather data
+function displayWeatherData(data, forecastData) {
+  // Location and date
+  locationElement.innerText = `${data.name}, ${data.sys.country}`;
+  const date = new Date();
+  dateElement.innerText = date.toDateString();
 
-function getWeather() {
-  var requestUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    inputValue.value +
-    "&appid=118c7f089309d40df508ea89da910c9e&units=imperial";
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      lat = data.coord.lat;
-      lon = data.coord.lon;
-      console.log(lat, lon)
+  // Weather icon
+  const iconCode = data.weather[0].icon;
+  const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+  weatherIconElement.innerHTML = `<img src="${iconUrl}" alt="${data.weather[0].description}" />`;
 
-      var tempValue = data["main"]["temp"];
-      var descImg =
-        "<img src =http://openweathermap.org/img/wn/" +
-        data["weather"][0]["icon"] +
-        ".png>";
-      var humidityValue = data["main"]["humidity"];
-      var windValue = data["wind"]["speed"];
+  // Temperature, wind, and humidity
+  const fahrenheitTemp = data['main']['temp'];
+  temperatureElement.innerHTML = "temp: " + fahrenheitTemp;
+  windElement.innerText =  data['wind']['speed'];
+  humidityElement.innerText = data['main']['humidity'];
 
-      currentTemp.innerHTML = "temp: " + tempValue;
-      currentDesc.innerHTML = descImg;
-      currentHumidity.innerHTML = "Humidity: " + humidityValue + " %";
-      currentWind.innerHTML = "Wind: " + windValue + " MPH";
-      console.log(currentWind);
-    });
-}
+  // Weather description
+  weatherDescriptionElement.innerText = `${data.weather[0].description}`;
 
-searchBtn.addEventListener("click", getWeather);
-
-function getApi() {
-  var requestUrl5 = 'https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=118c7f089309d40df508ea89da910c9e&units=imperial';
-//   var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-  // var requestUrl5 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-  console.log(requestUrl5);
-  fetch(requestUrl5)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      for (i = 1; i < 16; i += 1) {
-        for (i = 1; i < 16; i += 1) {
-          document.getElementById("temp" + (i + 1)).innerHTML =
-            "temp: " + data["list"][i]["main"]["temp"];
-        }
-      }
-      for (i = 1; i < 16; i += 1) {
-        document.getElementById("day" + (i + 1)).innerHTML =
-          "Date: " + data["list"][i]["dt_txt"].split(" ")[0];
-        for (i = 1; i < 16; i += 1) {
-          document.getElementById("day" + (i + 1)).innerHTML =
-            "Date: " + dayjs(data["list"][i]["dt_txt"]).format("M/D/YYYY");
-        }
-      }
-      for (i = 1; i < 16; i += 1) {
-        for (i = 1; i < 16; i += 1) {
-          document.getElementById("wind" + (i + 1)).innerHTML =
-            "wind: " + data["list"][i]["wind"]["speed"] + " MPH";
-        }
-      }
-      for (i = 1; i <16; i += 1) {
-        for (i = 1; i < 16; i += 1) {
-          document.getElementById("humidity" + (i + 1)).innerHTML =
-            "humidity: " + data["list"][i]["main"]["humidity"] + " %";
-        }
-      }
-      for (i = 1; i < 16; i += 1) {
-        for (i = 1; i < 16; i += 1) {
-          document.getElementById("img" + (i + 1)).src =
-            "http://openweathermap.org/img/wn/" +
-            data["list"][i]["weather"][0]["icons"] +
-            ".png";
-        }
-      }
-    
-
-
-var nameValue = data['city']['name'];
-        nameEl.innerHTML = nameValue;
-        localStorage.setItem('City', City);
-        console.log(City)
-         searchFunction()
-         
-      });
+  // Forecast
+  let forecastHtml = '';
+  for (let i = 0; i < forecastData.list.length; i++) {
+    const forecast = forecastData.list[i];
+    const forecastDate = new Date(forecast.dt_txt);
+    if (forecastDate.getHours() === 12) {
+      const forecastIconCode = forecast.weather[0].icon;
+      const forecastIconUrl = `https://openweathermap.org/img/w/${forecastIconCode}.png`;
+      const forecastTemperature = ((forecast.main.temp )).toFixed(1);
+      forecastHtml += `<div class="forecast-item">
+                          <div class="forecast-date">${forecastDate.toDateString()}</div>
+                          <div class="forecast-icon"><img src="${forecastIconUrl}" alt="${forecast.weather[0].description}" /></div>
+                          <div class="forecast-temperature">${forecastTemperature}</div>
+                        </div>`;
+    }
   }
+  forecastElement.innerHTML = forecastHtml;
+   }
 
-searchBtn.addEventListener("click", getApi);
-
-
-function removeHide() {
-  unhideDiv.classList.remove("hide");
-  unhideDiv1.classList.remove("hide");
-  unhideDiv2.classList.remove("hide");
-  unhideDiv3.classList.remove("hide");
-  unhideDiv4.classList.remove("hide");
-  unhideDiv5.classList.remove("hide");
-  hideDiv.classList.add("hide");
-}
-
-searchBtn.addEventListener("click", removeHide);
-
-
-
-function searchFunction(data) {
-
-    City.push($('#inputValue').val()); 
-    $('#inputValue').val(""); 
-    $('#history').text(""); 
-  
-    $.each(City, function(index, value) {
-      $('#history').append("<li class='historyItem'  onclick='addtoinputValue(" + index + ")'>" + value + '</li>');
-    });
-  }
-  
-  function addtoinputValue(id) {
-    $('#inputValue').val(City[id]);
-  }
-
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-//
-
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
-// register for open weather api **
-
-// save api key in a var **
-
-// make click event for search button(still not working)
-
-// take data from search box after we click button and save to a var
-//  call a fetch request on weather api
-// parse return data from fetch request
-// pull weather icons from open weather
